8f60079dd7cf68965e62f3b87a796212e7c925c0
Next Developer <information.instaforex@gmail.com>
2025-09-19 14:53:49 +0600

    seller model

--- src/db/modal/Seller.js
+++ src/db/modal/Seller.js
@@ -1,23 +1,73 @@
 const { default: mongoose } = require("mongoose");
+const UserRoles = require("../../domain/UserRole");
+const AccountStatus = require("../../domain/AccountStatus");
 
-const SellerSchema = new mongoose.Schema({
-    sellerName:{
+const sellerSchema = new mongoose.Schema({
+    sellerName: {
         type: String,
         required: true
     },
-    mobile:{
+    mobile: {
         type: Number,
         required: true,
         unique: true
     },
-    email:{
-         type: String,
+    email: {
+        type: String,
         required: true,
         unique: true
     },
-    password:{
+    password: {
         type: String,
         required: true,
         select: false
+    },
+    businessDetails: {
+        businessName: {
+            type: String,
+        },
+        businessEmail: {
+            type: String,
+        },
+        businessPhone: {
+            type: String,
+        },
+        businessAddress: {
+            type: String,
+        }
+    },
+    bankDetails: {
+        accountNumber: {
+            type: String
+        },
+        accountHolderName: {
+            type: String
+        },
+        bankName: {
+            type: String
+        },
+        ifscCode: {
+            type: String
+        }
+    },
+    pickUpAddress: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address"
+    },
+    GSTIN: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: [UserRoles.SELLER],
+        default: UserRoles.SELLER
+    },
+    accountStatus: {
+        type: String,
+        enum: [AccountStatus.PENDING_VERIFICATION, AccountStatus.ACTIVE, AccountStatus.SUSPENDED, AccountStatus.DEACTIVATED, AccountStatus.BANNED, AccountStatus.CLOSED,],
+        default: AccountStatus.PENDING_VERIFICATION,
     }
-})
+}, { timestamps: true })
+
+module.exports = mongoose.model("Seller", sellerSchema)
